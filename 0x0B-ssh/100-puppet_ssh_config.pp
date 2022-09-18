dd954d2d2404ba21e6b5649185c564341605e83c
belovetech <belovetech@gmail.com>
2022-09-18 14:31:25 +0100

    Fix puppet-lint error

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,6 +3,6 @@
 
 exec { 'echo':
   path    => 'usr/bin:/bin',
-  command => 'echo "IdentityFile ~/.ssh/school\n PasswordAuthentication no" >> etc/ssh/ssh_config',
-  returns => [0,1]
+  command => 'echo "    IdentityFile ~/.ssh/holberton\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1],
 }

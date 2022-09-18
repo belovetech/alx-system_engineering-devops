1adddeea9a271766b6c09fd1bfc6c09b34ef2a14
belovetech <belovetech@gmail.com>
2022-09-18 14:23:18 +0100

    Changes SSH config file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,5 +4,5 @@
 exec { 'echo':
   path    => 'usr/bin:/bin',
   command => 'echo "IdentityFile ~/.ssh/school\n PasswordAuthentication no" >> etc/ssh/ssh_config',
-  returns  => [0,1]
+  returns => [0,1]
 }

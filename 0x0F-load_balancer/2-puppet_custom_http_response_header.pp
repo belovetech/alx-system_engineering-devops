c4bdac6869540fd09cc9c41b8a1b0879096c9708
belovetech <belovetech@gmail.com>
2022-09-26 22:13:18 +0100

    [Feat]: Install Nginx with HTTP header with puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,27 @@
+# Install Nginx with HTTP header
+
+exec { 'update':
+  provider => shell,
+  command  => 'apt-get -y update',
+  before   => Exec['instal nginx']
+}
+
+
+exec { 'install Nginx':,
+  provide => shell,
+  command => 'apt-get -y install nginx',
+  before  =>   Exec['add-header']
+}
+
+exec { 'add-header':
+  provide     => shell,
+  environment => ["HOST=${hostname}"],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;
+  \n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart Nginx']
+}
+
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart'
+}

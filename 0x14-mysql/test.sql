cfec3de7c5309011fe9b64839c34b600c1530796
belovetech <belovetech@gmail.com>
2022-10-14 11:41:52 +0100

    [Update]: master-slave config

--- 0x14-mysql/test.sql
+++ 0x14-mysql/test.sql
@@ -13,3 +13,20 @@ INSERT INTO nexus6 (id, name) VALUES (1, "Beloved");
 CREATE USER IF NOT EXISTS replica_user@'%' IDENTIFIED BY 'replica_passwd';
 GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%' IDENTIFIED BY 'replica_passwd';
 GRANT SELECT ON mysql.user TO 'holberton_user'@'localhost';
+
+
+CHANGE REPLICATION SOURCE TO 
+SOURCE_HOST='%',
+SOURCE_USER='replica_user', SOURCE_PASSWORD='replica_passwd', SOURCE_LOG_FILE='mysql-bin.000002', SOURCE_LOG_POS=154;
+
+CHANGE MASTER TO
+MASTER_HOST='35.153.143.199',
+MASTER_USER='replica_user',
+MASTER_PASSWORD='replica_passwd',
+MASTER_LOG_FILE='mysql-bin.000003',
+MASTER_LOG_POS=2943;
+
+CHANGE MASTER TO
+  RELAY_LOG_FILE='mysql-relay-bin.000002',
+  RELAY_LOG_POS=4025;
+START SLAVE SQL_THREAD;

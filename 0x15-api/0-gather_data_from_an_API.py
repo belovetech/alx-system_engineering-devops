66b5c9e0d1d6362cb2ef17efd0edb35805afc579
belovetech <belovetech@gmail.com>
2022-10-17 17:51:06 +0100

    [Update]: refactor codes

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,31 +5,19 @@ import sys
 
 
 if __name__ == '__main__':
-    # url = "https://jsonplaceholder.typicode.com"
+    url = "https://jsonplaceholder.typicode.com"
     employer_id = sys.argv[1]
 
-    res1 = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}".format(employer_id))
+    users = requests.get("{}/users/{}".format(url, employer_id)).json()
+    todos = requests.get(url + "/todos", params={"userId": employer_id}).json()
 
-    res2 = requests.get(
-        "https://jsonplaceholder.typicode.com/todos")
+    completed = [data.get('title')
+                 for data in todos if data.get('completed') is True]
 
-    total_number_of_task = 0
-    number_of_done_task = 0
-    employer_name = res1.json().get('name')
-    titles = []
-
-    for data in res2.json():
-        if data.get('userId') == int(employer_id):
-            total_number_of_task += 1
-
-            if data.get('completed') is True:
-                number_of_done_task += 1
-                titles.append(data.get('title'))
-        else:
-            continue
+    total_number_of_task = len(todos)
+    number_of_done_task = len(completed)
+    employer_name = users.get('name')
 
     print("Employee {} is done with tasks({}/{}):".format(employer_name,
           number_of_done_task, total_number_of_task))
-    for title in titles:
-        print("\t {}".format(title))
+    [print("\t {}".format(title)) for title in completed]

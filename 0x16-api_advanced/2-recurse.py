4e407b1e1e18557dc86887455eac2283c7e9d917
belovetech <belovetech@gmail.com>
2022-11-01 20:08:30 +0100

    [Feat]: ecursive function that queries the Reddit API and returns a list containing the titles

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Fetch titles of top 10 hot posts subreddit API"""
+from urllib import response
+import requests
+
+
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """Get titles of top 10 hot posts
+    Args:
+        subreddit (str): subreddit
+        hot_list (list): hot list
+    Returns:
+        (str): titles of top 10 posts
+    """
+    headers = {'User-agent': 'myAPI'}
+    params = {
+        'after': after,
+        'count': count,
+        'limit': 100
+    }
+    base_url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    res = requests.get(base_url, headers=headers,
+                       params=params, allow_redirects=False)
+    if res.status_code == 404:
+        return None
+
+    results = res.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for post in results.get("children"):
+        hot_list.append(post.get("data").get("title"))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list

7756146b334d9525be6016ff9fd3313ff09d537b
belovetech <belovetech@gmail.com>
2022-11-01 13:46:32 +0100

    [Feat]: Fetch titles of top 10 hot posts of subreddit API

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Fetch titles of top 10 hot posts subreddit API"""
+import requests
+
+
+def top_ten(subreddit):
+    """Get titles of top 10 hot posts
+    Args:
+        subreddit (str): subreddit
+    Returns:
+        (str): titles of top 10 posts
+    """
+    headers = {'User-agent': 'myAPI'}
+    listing = 'top'
+    limit = 10
+    timeframe = 'month'
+
+    try:
+        base_url = 'https://www.reddit.com/r/{}/{}.json?limit={}&t={}'.format(
+                                subreddit, listing, limit, timeframe)
+        res = requests.get(base_url, headers=headers)
+        for post in res.json()['data']['children']:
+            for key, value in post['data'].items():
+                if key == 'title':
+                    print(value)
+    except Exception:
+        print(None)

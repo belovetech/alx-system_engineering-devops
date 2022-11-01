026fddf409357daaa67a4c410e987be8bcd6862e
belovetech <belovetech@gmail.com>
2022-11-01 13:22:43 +0100

    [Feat]: Fetch number of subscribers from subreddit API

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return number of subscribers
+    Args:
+        subreddit (str): subreddit
+    Returns:
+        (int): Number of the subscribers
+    """
+    headers = {'User-agent': 'myAPI'}
+    listing = 'top'
+    limit = 100
+    timeframe = 'month'
+
+    try:
+        base_url = 'https://www.reddit.com/r/{}/{}.json?limit={}&t={}'.format(
+                                subreddit, listing, limit, timeframe)
+        res = requests.get(base_url, headers=headers)
+        for post in res.json()['data']['children']:
+            for key, value in post['data'].items():
+                if key == 'subreddit_subscribers':
+                    return value
+    except Exception:
+        return 0
